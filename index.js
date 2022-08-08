56147ac71e630c07c18a7f26aef1c32628696ca9
Mohamd Imad <medoimad@hotmail.com>
2022-08-08 08:51:14 -0400

    Add a for loop in the js document

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// alert("hi");
+// A For loop breakdown:
+// -The first part is the items
+// -The second part is the duration of the loop
+// -The third part is the condition of how much chnages each time
+// -The fourth part is code executed
+
+for (x=1; (x<document.getElementsByClassName("notes_set").length+1); x=x+1) { console.log('hi');}
