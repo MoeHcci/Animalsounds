bdbaac1dd391461ddec8ea246a6d721f2a646bd9
Mohamd Imad <medoimad@hotmail.com>
2022-08-08 09:35:46 -0400

    Add an event listener to a for loop

--- index.js
+++ index.js
@@ -4,4 +4,17 @@
 // -The third part is the condition of how much chnages each time
 // -The fourth part is code executed
 
-for (x=1; (x<document.getElementsByClassName("notes_set").length+1); x=x+1) { console.log('hi');}
+// The EventListener:
+// The below addEventListener has an anonymous function
+
+// The For loop & EventListener:
+// 1. The for loop will run for the times "notes_set" has been mentioned then because of the special conditions of the
+// eventlistener method, each time we click on [x] the clicked on [x] is activated because that specifci button is also activated
+// For example: [x] = 2 is the alligator button
+
+for ( var x=0; (x<document.getElementsByClassName("notes_set").length); x=x+1) {
+
+document.getElementsByClassName("notes_set")[x].addEventListener("click", function(){
+  console.log('hi');
+})
+}
