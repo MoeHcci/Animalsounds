c563fd9c1c640e83beb0e1d2d7bd9e627863359d
Mohamd Imad <medoimad@hotmail.com>
2022-08-10 08:39:59 -0400

    Play the sounds using the switch statement but keep the if statemtn and comment it out for now

--- index.js
+++ index.js
@@ -66,59 +66,110 @@ for (var x = 0;
 
 
       // Playing Sound Using the If statements
-      if (this.innerHTML == "lions") {
-        // var was used instead of a function, because functions did not work
-        var animal1 = new Audio('lion.mp3').play();
-
-      } else if (this.innerHTML == "alligator") {
-        var animal2 = new Audio('alligator.mp3').play();
-
-      } else if (this.innerHTML == "camel") {
-        var animal3 = new Audio('camel.mp3').play();
-
-      } else if (this.innerHTML == "cat") {
-        var animal4 = new Audio('cat.mp3').play();
-
-      } else if (this.innerHTML == "cow") {
-        var animal5 = new Audio('cow.mp3').play();
-
-      } else if (this.innerHTML == "dog") {
-        var animal6 = new Audio('dog.mp3').play();
-
-      } else if (this.innerHTML == "dolphine") {
-        var animal7 = new Audio('dolphin.mp3').play();
-
-      } else if (this.innerHTML == "eagle") {
-        var animal8 = new Audio('eagle.mp3').play();
-
-      } else if (this.innerHTML == "elephants") {
-        var animal9 = new Audio('elephant.mp3').play();
-
-      } else if (this.innerHTML == "goat") {
-        var animal10 = new Audio('goat.mp3').play();
-
-      } else if (this.innerHTML == "gorilla") {
-        var animal11 = new Audio('gorilla.mp3').play();
-
-      } else if (this.innerHTML == "horse") {
-        var animal12 = new Audio('horse.mp3').play();
-
-      } else if (this.innerHTML == "monkeys") {
-        var animal13 = new Audio('monkey.mp3').play();
-
-      } else if (this.innerHTML == "rooster") {
-        var animal14 = new Audio('rooster.mp3').play();
-
-      } else if (this.innerHTML == "wildbeast") {
-        var animal15 = new Audio('wildbeast.mp3').play();
-      } else {
-        // This will trigger when none of the buttons has been pressed
-        // and the innerHTML of this equals to something not in the buttons
-        alert("Place holder, check the If statement");
-
-      }
+      // Use different var each time to keep things orginized
+      // if (this.innerHTML == "lions") {
+      //   // var was used instead of a function, because functions did not work
+      //   var animal1 = new Audio('lion.mp3').play();
+      //
+      // } else if (this.innerHTML == "alligator") {
+      //   var animal2 = new Audio('alligator.mp3').play();
+      //
+      // } else if (this.innerHTML == "camel") {
+      //   var animal3 = new Audio('camel.mp3').play();
+      //
+      // } else if (this.innerHTML == "cat") {
+      //   var animal4 = new Audio('cat.mp3').play();
+      //
+      // } else if (this.innerHTML == "cow") {
+      //   var animal5 = new Audio('cow.mp3').play();
+      //
+      // } else if (this.innerHTML == "dog") {
+      //   var animal6 = new Audio('dog.mp3').play();
+      //
+      // } else if (this.innerHTML == "dolphine") {
+      //   var animal7 = new Audio('dolphin.mp3').play();
+      //
+      // } else if (this.innerHTML == "eagle") {
+      //   var animal8 = new Audio('eagle.mp3').play();
+      //
+      // } else if (this.innerHTML == "elephants") {
+      //   var animal9 = new Audio('elephant.mp3').play();
+      //
+      // } else if (this.innerHTML == "goat") {
+      //   var animal10 = new Audio('goat.mp3').play();
+      //
+      // } else if (this.innerHTML == "gorilla") {
+      //   var animal11 = new Audio('gorilla.mp3').play();
+      //
+      // } else if (this.innerHTML == "horse") {
+      //   var animal12 = new Audio('horse.mp3').play();
+      //
+      // } else if (this.innerHTML == "monkeys") {
+      //   var animal13 = new Audio('monkey.mp3').play();
+      //
+      // } else if (this.innerHTML == "rooster") {
+      //   var animal14 = new Audio('rooster.mp3').play();
+      //
+      // } else if (this.innerHTML == "wildbeast") {
+      //   var animal15 = new Audio('wildbeast.mp3').play();
+      // } else {
+      //   // This will trigger when none of the buttons has been pressed
+      //   // and the innerHTML of this equals to something not in the buttons
+      //   alert("Place holder, check the If statement");
+      //
+      // }
 
       // Playing Sound Using the switch statements
+      // Use different var each time to keep things orginized
+      switch(this.innerHTML) {
+        case this.innerHTML = "lions":
+        var animal1 = new Audio('lion.mp3').play();
+        break
+        case this.innerHTML = "alligator":
+        var animal2 = new Audio('alligator.mp3').play();
+        break
+        case this.innerHTML = "camel":
+        var animal3 = new Audio('camel.mp3').play();
+        break
+        case this.innerHTML = "cat":
+        var animal4 = new Audio('cat.mp3').play();
+        break
+        case this.innerHTML = "cow":
+        var animal5 = new Audio('cow.mp3').play();
+        break
+        case this.innerHTML = "dog":
+        var animal6 = new Audio('dog.mp3').play();
+        break
+        case this.innerHTML = "dolphine":
+        var animal7 = new Audio('dolphin.mp3').play();
+        break
+        case this.innerHTML = "eagle":
+        var animal8 = new Audio('eagle.mp3').play();
+        break
+        case this.innerHTML = "elephants":
+        var animal9 = new Audio('elephant.mp3').play();
+        break
+        case this.innerHTML = "goat":
+        var animal10 = new Audio('goat.mp3').play();
+        break
+        case this.innerHTML = "gorilla":
+        var animal11 = new Audio('gorilla.mp3').play();
+        break
+        case this.innerHTML = "horse":
+        var animal12 = new Audio('horse.mp3').play();
+        break
+        case this.innerHTML = "monkeys":
+        var animal13 = new Audio('monkeys.mp3').play();
+        break
+        case this.innerHTML = "rooster":
+        var animal14 = new Audio('rooster.mp3').play();
+        break
+        case this.innerHTML = "wildbeast":
+        var animal15 = new Audio('wildbeast.mp3').play();
+        break
+        default:
+        alert("Place holder, check the switch statement");
+      }
 
 
   }
